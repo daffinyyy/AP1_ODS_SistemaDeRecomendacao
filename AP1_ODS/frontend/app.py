8c2159272adc381e79e6c14f211b318b6dcacab5
Aline Daffiny Ferreira Gomes <149741824+daffinyyy@users.noreply.github.com>
2025-09-26 12:08:22 -0400

    update dataset

--- AP1_ODS/frontend/app.py
+++ AP1_ODS/frontend/app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import requests
 
-st.title("Sistema de Recomendação de Cerveja")
+st.title("Sistema de Recomendação de Livros")
 
 user_id = st.number_input("Digite o ID do usuário:", min_value=1, max_value=3, step=1)
 
 st.button("Recomendar")
-    #response = requests.get(f"http://127.0.0.1:5000/recomendar/{user_id}")
+    #response = requests.get(f"")
     # if response.status_code == 200:
     #     recomendacoes = response.json()
-    #     st.write("Filmes recomendados:")
+    #     st.write("Livros recomendados:")
     #     for r in recomendacoes:
     #         st.write(f"- {r['title']} ({r['genre']})")
     # else:
